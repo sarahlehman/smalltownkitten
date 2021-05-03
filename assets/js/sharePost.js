b2e21c000dec85f2cdfb4742a340b01279cdf93f
Justin Santoro <jzsantoro14@gmail.com>
2021-05-02 22:43:07 -0400

    fix post sharing

--- assets/js/sharePost.js
+++ assets/js/sharePost.js
@@ -1,17 +1,17 @@
+const sharebtncontainer = document.getElementById("sharecontainer")
 const sharebtn = document.getElementById("sharebtn")
 const shareData = {
     title: document.title,
     url: document.location.href
 }
-if (sharebtn) {
-    if (!navigator.share) {
-        //no share for u
-        sharebtn.classList.add("is-hidden");
-    }
-    sharebtn.addEventListener("click", function(){
-        navigator.share(shareData).then(() => {
-            console.log('sharing is caring');
-          })
-          .catch(console.error);
-    });
+
+if (!navigator.share) {
+    //no share for u
+    sharebtncontainer.classList.add("is-hidden");
 }
+sharebtn.addEventListener("click", function(){
+    navigator.share(shareData).then(() => {
+        console.log('sharing is caring');
+        })
+        .catch(console.error);
+});
